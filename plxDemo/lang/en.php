1f5304f51adf9b231c135303d8e62699cf30146a
gcyrillus <1832331+gcyrillus@users.noreply.github.com>
2022-06-22 13:58:10 +0200

    Add files via upload
    Permet de se connecter a l'administration en mode DEMO. ne necessite pas de creer de nouveau utilisateurs, le plugin les créé à la volée si actif.

--- plxDemo/lang/en.php
+++ plxDemo/lang/en.php
@@ -1,5 +1,7 @@
 <?php
 $LANG = array(
-	'L_DEMO_ONLY'			=> 'Demo Only',
+	'L_DEMO_ONLY'			=> 'Demo Only , Update Config Not Avalaible',
+	'L_DEMO_PROFILS'		=>'Connections Profils',
+	'L_DEMO_SINGLE_PASS'	=>'Common password',
 );
 ?>
--- plxDemo/lang/fr.php
+++ plxDemo/lang/fr.php
@@ -1,5 +1,7 @@
 <?php
 $LANG = array(
-	'L_DEMO_ONLY'			=> 'Mode Dèmo',
+	'L_DEMO_ONLY'			=> 'Mode Demo, Modifications Désactivées',
+	'L_DEMO_PROFILS'		=>'Profils de connexion',
+	'L_DEMO_SINGLE_PASS'	=>'Mot de passe commun',
 );
 ?>
